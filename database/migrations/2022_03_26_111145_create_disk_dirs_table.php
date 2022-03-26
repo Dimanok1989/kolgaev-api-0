326307345bd164361d24e6494de697a19bbfb33b
Dimanok1989 <kolgaev@gmail.com>
2022-03-26 11:14:20 +0300

    Model and migration disk dirs

---
+++ database/migrations/2022_03_26_111145_create_disk_dirs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disk_dirs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name')->comment('Наименование каталога');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disk_dirs');
+    }
+};
