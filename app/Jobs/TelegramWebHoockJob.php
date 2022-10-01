ec98274341a7be4f202dd017c97f397e77c39840
Dimanok1989 <kolgaev@gmail.com>
2022-10-01 07:43:32 +0300

    таймаут

--- app/Jobs/TelegramWebHoockJob.php
+++ app/Jobs/TelegramWebHoockJob.php
@@ -15,6 +15,13 @@ class TelegramWebHoockJob implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout = 1200;
+
+    /**
      * Create a new job instance.
      *
      * @return void
